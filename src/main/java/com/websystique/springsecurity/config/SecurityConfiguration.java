504607298906223a625d54a6beb1b173f86244ea
lwk <qwfys200@qq.com>
2017-06-16 14:58:16 +0800

    调整项目结构

--- src/main/java/com/websystique/springsecurity/config/SecurityConfiguration.java
+++ src/main/java/com/websystique/springsecurity/config/SecurityConfiguration.java
@@ -1,9 +1,8 @@
-package com.websystique.springsecurity.configuration;
+package com.websystique.springsecurity.config;
 
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -19,7 +18,6 @@ import org.springframework.security.web.authentication.rememberme.PersistentToke
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-	@Qualifier("customUserDetailsService")
 	UserDetailsService userDetailsService;
 	
 	@Autowired
@@ -33,14 +31,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 	  http.authorizeRequests()
-	  	.antMatchers("/", "/home").permitAll()
-	  	.antMatchers("/admin/**").access("hasRole('ADMIN')")
-	  	.antMatchers("/db/**").access("hasRole('ADMIN') and hasRole('DBA')")
-	  	.and().formLogin().loginPage("/login")
-	  	.usernameParameter("ssoId").passwordParameter("password")
-	  	.and().rememberMe().rememberMeParameter("remember-me").tokenRepository(persistentTokenRepository()).tokenValiditySeconds(86400)
-	  	.and().csrf()
-	  	.and().exceptionHandling().accessDeniedPage("/Access_Denied");
+        .antMatchers("/", "/home").permitAll()
+        .antMatchers("/admin/**").access("hasRole('ADMIN')")
+        .antMatchers("/db/**").access("hasRole('ADMIN') and hasRole('DBA')")
+        .and().formLogin().loginPage("/login")
+        .usernameParameter("ssoId").passwordParameter("password")
+        //.and().rememberMe().rememberMeParameter("remember-me").tokenRepository(persistentTokenRepository()).tokenValiditySeconds(86400)
+        .and().csrf()
+        .and().exceptionHandling().accessDeniedPage("/Access_Denied");
 	  	
 	}
 	
