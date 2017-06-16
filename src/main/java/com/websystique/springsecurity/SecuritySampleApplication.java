c4c85c38cc809f39d1d2b2ff656c26875c553f0d
lwk <qwfys200@qq.com>
2017-06-16 09:47:44 +0800

    修改包路径与artifactId

--- src/main/java/com/websystique/springsecurity/SecuritySampleApplication.java
+++ src/main/java/com/websystique/springsecurity/SecuritySampleApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.websystique.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
